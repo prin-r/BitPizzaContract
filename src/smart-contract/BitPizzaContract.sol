844c552ab4da7851ecc24c82dd4a937491bf31a4
prin-r <lang_zane@hotmail.com>
2018-07-29 14:43:35 +0700

    update contracts

--- src/smart-contract/BitPizzaContract.sol
+++ src/smart-contract/BitPizzaContract.sol
@@ -10,7 +10,7 @@ contract BitPizzaContract is Ownable {
     uint32 public numCreatedTickets = 0;
     uint32 public numClaimedTickets = 0;
     
-    mapping (string => bool) private tickets;
+    mapping (bytes32 => bool) private tickets;
     
     event createTicketEvent(uint32 _numCreatedTickets, uint32 _numClaimedTickets, string _message);
     event claimTicketEvent(uint32 _numCreatedTickets, uint32 _numClaimedTickets, string _message);
@@ -26,30 +26,10 @@ contract BitPizzaContract is Ownable {
         return (result, result.length);
     }
     
-    function testByte2String(bytes32 _bytes32) public pure returns(string) {
-        string memory b2s = bytes32ToString(_bytes32);
-        return (b2s);
-    }
-    
-    function testString2Bytes(string _nakeSeed) public pure returns(bytes32,string) {
-        bytes32 seed = keccak256(bytes(_nakeSeed));
-        string memory b2s = bytes32ToString(seed);
-        return (seed,b2s);
-    }
-    
-    function bytes32ToString(bytes32 x) private pure returns (string) {
-        bytes memory bytesArray = new bytes(32);
-        for (uint16 i; i < 32; i++) {
-            bytesArray[i] = x[i];
-        }
-        return string(bytesArray);
-    }
-    
     function createTicket(bytes32 _bytesSeed) public onlyBitStudio() {
-        string memory seed = bytes32ToString(_bytesSeed);
-        require(! tickets[seed]);
+        require(! tickets[_bytesSeed]);
         
-        tickets[seed] = true;
+        tickets[_bytesSeed] = true;
         numCreatedTickets = numCreatedTickets.add(1);
         
         emit createTicketEvent(numCreatedTickets, numClaimedTickets, "creation successful");
@@ -57,11 +37,10 @@ contract BitPizzaContract is Ownable {
     
     function claimTicket(string _nakeSeed) public onlyPizzaSeller() onlyProperString(_nakeSeed) {
         bytes32 bytesSeed = keccak256(bytes(_nakeSeed));
-        string memory seed = bytes32ToString(bytesSeed);
         
-        require(tickets[seed]);
+        require(tickets[bytesSeed]);
         
-        tickets[seed] = false;
+        tickets[bytesSeed] = false;
         numClaimedTickets = numClaimedTickets.add(1);
         
         emit claimTicketEvent(numCreatedTickets, numClaimedTickets, "claiming successful");
@@ -69,7 +48,6 @@ contract BitPizzaContract is Ownable {
     
     function ticketStatus(string _nakeSeed) public view onlyProperString(_nakeSeed) returns(bool) {
         bytes32 bytesSeed = keccak256(bytes(_nakeSeed));
-        string memory seed = bytes32ToString(bytesSeed);
-        return tickets[seed];
+        return tickets[bytesSeed];
     }
 }
